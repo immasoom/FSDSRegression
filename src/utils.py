0b04eaa8c68555eac20463c2943e352292cb898c
Syed Haque <masoom.haque@gmail.com>
2025-09-09 19:32:34 +0530

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -22,3 +22,30 @@ def save_object(file_path, obj):  ###file_path of picke file, obj is the preproc
     except Exception as e:
         raise CustomException(e,sys)
     
+def evaluate_model(X_train,y_train,X_test,y_test,models):
+    try:
+        report = {}
+        for i in range(len(models)):
+            model = list(models.values())[i]
+
+            #Train model
+            model.fit(X_train,y_train)
+
+            #predict Training data
+            #y_train_pred = model.predict(X_train)
+
+            #predict testing data
+            y_test_pred = model.predict(X_test)
+
+            # Get R2 scores for train and test data
+            #train_model_score = r2_score(y_train,y_train_pred)
+            test_model_score = r2_score(y_test,y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+        
+    
+    except Exception as e:
+        raise CustomException(e,sys)
+    
